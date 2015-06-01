980c2c109806f4e7516282a24b62e99babf1fd36
Szymon Wanot <szwanot@gmail.com>
2015-06-01 18:47:28 +0200

    new grammar for parser

--- Lexer/Lexer/SyntaxAnalizer.py
+++ Lexer/Lexer/SyntaxAnalizer.py
@@ -16,7 +16,7 @@ class SyntaxAnalizer:
         file.close()
         self.lexer.input_insert(file_content)
         self.next_token()
-        return self.expression_tree_build()
+        return self.W()
     
     def next_token(self):
         try:
@@ -26,71 +26,70 @@ class SyntaxAnalizer:
         except LexExeption as e:
             print(e)
     
-    def match_grammar(self, type):
-        if self.current_token.type == type:
-            val = self.current_token.val
-            self.next_token()
-            return val
-    
-    def expression_tree_build(self):
-        if self.current_token.type is None:
-            return ''
-        else:
-            return self.match_expession()
-
-    def match_expession(self):
-        lval = self.match_term()
-
-        if self.current_token.type == 'plus':
-            self.match_grammar('plus')
-            op = lambda a, b : a + b
-        elif self.current_token.type == 'minus':
-            self.match_grammar('minus')
-            op = lambda a, b: a - b
-        else:
-            print ('returning lval = {0} '.format(lval))
-            return lval
-         
-        rval = self.match_expession()
-        print ('lval = {0}, rval = {1}, res = {2}'.format( lval, rval, op(lval, rval)))
-        return op(lval, rval)   
-    
-    def match_term(self):
-        lval = self.match_factor()
-        if self.current_token.type == 'multi':
-            self.match_grammar('multi')
-            op = lambda a, b: a * b
-        elif self.current_token.type == 'div':
-            self.match_grammar('div')
-            op = lambda a, b: a / b
-        else:
-            return lval
-        
-        rval = self.match_term()
-        return op(lval, rval)
-
-    def match_factor(self):
-        if self.current_token.type == 'lp':
-            self.match_grammar('lp')
-            val = self.match_expession()
-            self.match_grammar('rp')
-            return val
-        elif self.current_token.type == 'number':
-            return int(self.match_grammar('number'))
+    def match_grammar_token(self, type):
+        try:
+            if self.current_token.type == type:
+                val = self.current_token.val
+                print('value {0} type {1}'.format(self.current_token.val, self.current_token.type))
+                self.next_token()
+                return val
+            else:
+                raise ValueError("current token {0} expected {1}".format(self.current_token.type, type))
+        except ValueError as e:
+            print(e)
+    #   W ->  S X
+    def W(self):
+        self.S()
+        self.X()
  
+    #   X -> + S X
+    #   X -> - S X
+    #   X -> eps
+    def X(self):
+        if self.current_token.type == 'plus' or self.current_token.type == 'minus':
+            self.match_grammar_token(self.current_token.type)
+            self.S()
+            self.X()
+
+    #   S -> C Y
+    def S(self):
+        self.C()
+        self.Y()
+    #   Y -> * C Y
+    #   Y -> / C Y
+    #   Y -> eps
+    def Y(self):
+       if self.current_token.type == 'multi' or self.current_token.type == 'div':
+           self.match_grammar_token(self.current_token.type)
+           self.C()
+           self.Y()
+    #   C -> liczba
+    #   C -> identyfikator
+    #   C -> ( W )
+    def C(self):
+        if self.current_token.type == 'number' or self.current_token.type == 'identifier':
+            self.match_grammar_token(self.current_token.type)
+        elif self.current_token.type == 'lp':
+            self.match_grammar_token('lp')
+            self.W()
+            self.match_grammar_token('rp')
+
+    
 def main(file, tok):
     parser = SyntaxAnalizer(tok)
     parser.parse(file)
 
 if __name__ == '__main__':
     tokens = [
-    (r'\+',                         'plus'),
-    (r'\-',                         'minus'),
-    (r'\*',                         'multi'),
-    (r'\/',                         'div'),
-    (r'\(',                         'lp'),
-    (r'\)',                         'rp'),
-    (r'^\d+([.]|)',           'number')]
+    (r'^[^\d\W]\w*',  'identifier'),
+    (r'\+',           'plus'),
+    (r'\-',           'minus'),
+    (r'\*',           'multi'),
+    (r'\/',           'div'),
+    (r'\(',           'lp'),
+    (r'\)',           'rp'),
+    (r'^\d+([.]|)',   'number')
+    ]
 
     
     file_path = "file.txt"
